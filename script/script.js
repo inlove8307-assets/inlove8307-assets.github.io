082839cee92141832cb1afd6226c1e02aa5c8fef
Kim Taehoon <inlove8307@gmail.com>
2020-08-18 09:01:51 +0900

    update script

--- script/script.js
+++ script/script.js
@@ -29,8 +29,7 @@ javascript: window.load = function(){
   }
 
   function flickr(){
-    var regexp = [/\/photos\/[\w-_@]+\/[\d]+\//g, /\/photos\/[\w-_@]+\/[\d]+\/sizes\/[\w]+\//g, /\([\d]+ &times; [\d]+\)/g, /live.staticflickr.com\/[\d]+\/[\w-_@]+\.[\D]{3}/g]
-      , style = document.createElement('style');
+    var style = document.createElement('style');
 
     style.textContent = '\
     .anchor {position:absolute;top:4px;left:4px;z-index:10;padding:2px 4px;border:1px solid rgba(0, 0, 0, 0.5);border-radius:5px;background-color:rgba(255, 255, 255, 0.5);font-size:13px;line-height:1;color:rgb(0, 0, 0)!important;}\
@@ -42,8 +41,8 @@ javascript: window.load = function(){
         , parent = target.parentNode;
 
       if (target.tagName == 'A' && target.classList.contains('overlay') && !parent.dataset.fetch) {
-        requestData([location.origin, target.href.match(regexp[0])[0], 'sizes/'].join(''), 'text', function(text){
-          var array = [text.match(regexp[1]), text.match(regexp[2])]
+        requestData([location.origin, target.href.match(/\/photos\/[\w-_@]+\/[\d]+\//g)[0], 'sizes/'].join(''), 'text', function(text){
+          var array = [text.match(/\/photos\/[\w-_@]+\/[\d]+\/sizes\/[\w]+\//g), text.match(/\([\d]+ &times; [\d]+\)/g)]
             , link = array[0][0].match(/\/o\//g) ? array[0][0] : array[0][array[0].length-1]
             , size = array[0][0].match(/\/o\//g) ? array[1][0] : array[1][array[1].length-1]
             , anchor;
@@ -63,7 +62,7 @@ javascript: window.load = function(){
     document.addEventListener('click', function(event){
       if (event.target.classList.contains('anchor')) {
         requestData(event.target.getAttribute('href'), 'text', function(text){
-          var array = text.match(regexp[3])
+          var array = text.match(/live.staticflickr.com\/[\d]+\/[\w-_@]+\.[\D]{3}/g)
             , url = ['https://', array[array.length-1]].join('');
 
           requestData(url, 'blob', function(blob){
